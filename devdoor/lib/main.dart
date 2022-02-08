75f2e36e300170e6ac53d32706ece641dd1796b3
Matt Smith <mattdsmith228@gmail.com>
2022-02-08 17:21:55 -0500

    added base widgets

--- devdoor/lib/main.dart
+++ devdoor/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,22 +27,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Door Dev Tools'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +42,142 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //Slider Vars
+  double _currentSetpoint = 0;
+  double _pVal = 0;
+  double _iVal = 0;
+  double _dVal = 0;
 
-  void _incrementCounter() {
+  bool isConnected = false;
+
+  var channel = IOWebSocketChannel.connect(Uri.parse('ws://localhost:9001'));
+
+  @override
+  void initState() {
+    channel.stream.listen((event) {
+      setState(() {
+        isConnected = true;
+      });
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void updateSetpoint(double value) {
+    channel.sink.add(value.toString());
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentSetpoint = value;
+    });
+  }
+
+  void updatePval(double value) {
+    channel.sink.add(value.toString());
+    setState(() {
+      _pVal = value;
+    });
+  }
+
+  void updateIval(double value) {
+    channel.sink.add(value.toString());
+    setState(() {
+      _iVal = value;
+    });
+  }
+
+  void updateDval(double value) {
+    channel.sink.add(value.toString());
+    setState(() {
+      _dVal = value;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: SizedBox(
+          width: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 60,
+                    child: Center(child: Text("Websocket")),
+                    color: isConnected ? Colors.green : Colors.red,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 60,
+                    child: Center(child: Text("Limit A")),
+                    color: false ? Colors.green : Colors.red,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 60,
+                    child: Center(child: Text("Limit B")),
+                    color: false ? Colors.green : Colors.red,
+                  )
+                ],
+              ),
+              Text("Door Setpoint: ${_currentSetpoint.round()}"),
+              Slider(
+                value: _currentSetpoint,
+                max: 100,
+                label: _currentSetpoint.round().toString(),
+                onChanged: updateSetpoint,
+              ),
+              Text("Door Current: ${_currentSetpoint.round()}"),
+              SizedBox(
+                height: 50,
+                child: LinearProgressIndicator(
+                  value: _currentSetpoint * .01,
+                ),
+              ),
+              Text("PID Settings:"),
+              Row(children: [
+                SizedBox(width: 40, child: Text("P: ${_pVal.round()}")),
+                Expanded(
+                  child: Slider(
+                    value: _pVal,
+                    max: 100,
+                    label: _pVal.round().toString(),
+                    onChanged: updatePval,
+                  ),
+                ),
+              ]),
+              Row(children: [
+                SizedBox(width: 40, child: Text("I: ${_iVal.round()}")),
+                Expanded(
+                  child: Slider(
+                    value: _iVal,
+                    max: 100,
+                    label: _iVal.round().toString(),
+                    onChanged: updateIval,
+                  ),
+                ),
+              ]),
+              Row(children: [
+                SizedBox(width: 40, child: Text("D: ${_dVal.round()}")),
+                Expanded(
+                  child: Slider(
+                    value: _dVal,
+                    max: 100,
+                    label: _dVal.round().toString(),
+                    onChanged: updateDval,
+                  ),
+                ),
+              ]),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
